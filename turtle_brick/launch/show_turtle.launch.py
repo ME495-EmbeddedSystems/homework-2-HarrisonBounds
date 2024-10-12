c389fa1e3ef0bde3fb404b98dd975027c3b4162e
Harrison Bounds <harrison.bounds777@gmail.com>
2024-10-12 10:37:19 -0500

    Created packages, launch file, base and platform links

---
+++ turtle_brick/launch/show_turtle.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import ExecutableInPackage, FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[
+                {"robot_description" :
+                Command([ExecutableInPackage("xacro", "xacro"), " ",
+                        PathJoinSubstitution(
+                        [FindPackageShare("turtle_brick"), "turtle.urdf"])])}
+                        ]
+            ),
+        Node(
+                package="joint_state_publisher_gui",
+                executable="joint_state_publisher_gui"
+            ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d", PathJoinSubstitution([FindPackageShare("turtle_brick"), "view_robot.rviz"])]
+        )
+                        ])
---
+++ turtle_brick/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtle_brick'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml', 'urdf/turtle.urdf', 'launch/show_turtle.launch.py', 'config/view_robot.rviz']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='harrison-bounds',
+    maintainer_email='harrison.bounds777@gmail.com',
+    description='Turtle Catching a falling brick. Embedded Systems Homework 2',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
